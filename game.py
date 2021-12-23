101603956781e4a073526270473cc40071a69408
Preston <pgonza13@jhu.edu>
2021-12-23 13:38:36 -0700

    test

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ def make_move(map, player=0, row=0, col=0):
     print("   0  1  2")
     for count, row in enumerate(map):
         print(count, row) #print row index and row item
-    return map
+    return map #avoid mutability errors
 
 game = make_move(game)
 game = make_move(game, player=1, row=2, col = 2)

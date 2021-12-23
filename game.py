12faa64db74b90e55d4740e1b2751a79a31a8def
Preston <pgonza13@jhu.edu>
2021-12-23 13:29:11 -0700

    first commit

---
+++ game.py
@@ -0,0 +1,12 @@
+game = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+def make_move(map, player=0, row=0, col=0):
+    if player != 0:
+        map[row][col] = player
+    print("   0  1  2")
+    for count, row in enumerate(map):
+        print(count, row) #print row index and row item
+    return map
+
+game = make_move(game)
+game = make_move(game, player=1, row=2, col = 2)
